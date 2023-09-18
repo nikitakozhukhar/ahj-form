bc3f72e1e043ade8ed4d8f64c909c89ac0dfddd9
NikitaKx9 <nikitakx9@gmail.com>
2023-09-18 14:38:28 +0300

    popover changed

--- src/js/popover.js
+++ src/js/popover.js
@@ -35,20 +35,18 @@ export default class Popover {
 		console.log(element.offsetHeight);
 		console.log('top' + '=' + top);
 
+
 		popoverElement.style.left = left - 26 + 'px';
     	popoverElement.style.top = top - element.offsetHeight * 2.8 + 'px';
 
-		// return id
+		return id
 
 	}
 
 	removePopover(id) {
 		const popover = this._popover.find(p => p.id == id);
-
 		popover.element.remove();
-
-		this._popover = this._popover.filter(t => t !== id)
-
+		this._popover = this._popover.filter(t => t !== id);
 	}
 
 }
--- src/js/popoverApp.js
+++ src/js/popoverApp.js
@@ -2,7 +2,11 @@ import Popover from "./popover";
 
 const popoverBtn = document.querySelector('.btn-popover');
 
+
 const popover = new Popover();
+
+let actualPopover = []; //18.09
+
 
 popoverBtn.addEventListener('click', e => {
 	e.preventDefault();
@@ -11,5 +15,14 @@ popoverBtn.addEventListener('click', e => {
 	const bodyText = e.target.dataset.content;
 	const element = e.target;
 	
-	popover.showPopover(title, bodyText, element);
+	// popover.showPopover(title, bodyText, element);
+	actualPopover.push({
+		name: '',
+		id: popover.showPopover(title, bodyText, element),
+	});
+	// 
+	actualPopover.forEach(p => popover.removePopover(p.id));
+	// actualPopover = [];
+	
+	console.log(actualPopover)
 })
